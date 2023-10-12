87190222f0738753c0acc926ada42dc43e1ad874
Matthew Puryear <mpuryear99@gmail.com>
2023-10-12 13:20:35 -0400

    Analysis restructure and sentence splitting

---
+++ src/analyze_books.py
@@ -0,0 +1,178 @@
+import os
+import re
+from read_books import read_book, iter_book_content
+
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOKS_DIR = os.path.abspath(os.path.join(FILE_DIR, '..', 'books'))
+
+
+### The Murder on the Links
+BOOK_AC_THE_MURDER_ON_THE_LINKS = {
+    'Title':    'The Murder on the Links',
+    'Author':   'Agatha Christie',
+
+    'filepath': (fn := os.path.join(BOOKS_DIR, 'Agatha_Christie-The_Murder_on_the_Links.txt')),
+    'content':  read_book(fn)[1],
+
+    'detectives': {
+        'Hercule Poirot':       r"(?:Mr\.)? (?:Hercule (?:\sPoirot)? | Poirot)",
+        'Arthur Hastings':      r"(?:Captain\s)? (?:Arthur\s)? Hastings",
+        'Monsieur Giraud':      r"(?:M\.\s|Monsieur\s)? Giraud",
+        'Monsieur Hautet':      r"(?:M\.\s|Monsieur\s)? Hautet",
+        'Japp':                 r"Japp",    # Only there 6 times...
+    },
+    'suspects': {
+        "Madame Daubreuil (Jeanne Beroldy)": r"""
+            (?:Madame\s|Mademoiselle\s) Daubreuil |
+            (?:Madame\s|Mademoiselle\s)? (?:Jeanne (?:\sBeroldy)? | Beroldy)
+        """,
+        "Eloise Renauld":       r"Eloise (?:\sRenauld)?",
+        "Jack Renauld":         r"Jack   (?:\sRenauld)?",
+      # "Monsieur Lucien Bex":  r"(?:M\.\s)? (?:Lucien\s) Bex",
+      # "Dulcie Duveen":        r"Dulcie (?:\sDuveen)? | Cinderella",
+    },
+    'perpetrators': {
+        'Marthe Daubreuil':     r"Marthe (?:\sDaubreuil)?",
+    },
+    'victims': {
+        "Paul Renauld":         r"Paul (?:\sRenauld)?",
+    },
+    'crime': r"grave | golf (?:\scourse)? | stabbed",
+}
+
+
+### The Mysterious Affair at Styles
+BOOK_AC_THE_MYSTERIOUS_AFFAIR_AT_STYLES = {
+    'Title':    'The Mysterious Affair at Styles',
+    'Author':   'Agatha Christie',
+
+    'filepath': (fn := os.path.join(BOOKS_DIR, 'Agatha_Christie-The_Mysterious_Affair_at_Styles.txt')),
+    'content':  read_book(fn)[1],
+
+    'detectives': {
+        'Hercule Poirot':       r"(?:Mr\.)? (?:Hercule (?:\sPoirot)? | Poirot)",
+        'Arthur Hastings':      r"(?:Captain\s)? (?:Arthur\s)? Hastings",
+        'Japp':                 r"Japp",
+    },
+    'suspects': {
+        'John Cavendish':       r"John (?:\sCavendish)?",
+        'Mary Cavendish':       r"Mary (?:\sCavendish)?",
+        'Lawrence Cavendish':   r"Lawrence (?:\sCavendish)?",
+        'Cynthia Murdoch':      r"Cynthia (?:\sMurdoch)?",
+        'Dr. Baurenstein':      r"(?:Dr\.\s)? Bauerstein",
+    },
+    'perpetrators': {
+        'Alfred Inglethorp':    r"Alfred (?:\sInglethorp)?",
+        'Evelyn Howard':        r"Evelyn (?:\sHoward)?",
+    },
+    'victims': {
+        'Emily Inglethorp':     r"Emily (?:\sInglethorp)?",
+    },
+    'crime': r"strychnine | poison",
+}
+
+
+### The Hound of the Baskervilles
+BOOK_CD_THE_HOUND_OF_THE_BASKERVILLES = {
+    'Title':    'The Hound of the Baskervilles',
+    'Author':   'Conan Doyle',
+
+    'filepath': (fn := os.path.join(BOOKS_DIR, 'Conan_Doyle-The_Hound_of_the_Baskervilles.txt')),
+    'content':  read_book(fn)[1],
+
+    'detectives': {
+        'Sherlock Holmes':      r"Sherlock (?:\sHolmes)? | Holmes",
+        'Dr. John Watson':      r"(?:Dr\.\s) (?:John (?:\sWatson)? | Watson)",
+    },
+    'suspects': {
+        'Henry Baskerville':    r"(?:Sir\s)? Henry (?:\sBaskerville)?",
+        'James Mortimer':       r"(?:Dr\.\s)? (?:James (?:\sMortimer)? | Mortimer)",
+        'Laura Lyons':          r"Laura (?:\sLyons)? | Lyons",
+        'Beryl Stapleton':      r"(?:Miss\s|Mrs\.\s) Stapleton | Beryl (?:\sGarcia)",        # Tip: No 'Beryl Stapleton' in book
+        'The Barrymores':       r"(?:Mr\.\s|Mrs\.\s)? Barrymores?"
+    },
+    'perpetrators': {
+        'Jack Stapleton':       r"Jack (?:\sStapleton)? | (?<!Miss\s|Mrs\.\s) Stapleton",
+    },
+    'victims': {
+        'Charles Baskerville':  r"(?:Sir\s)? Charles (?:\sBaskerville)?",
+    },
+    'crime': r"heart | cardiac | exhaustion | fatal",
+}
+
+
+BOOKS = [
+    BOOK_AC_THE_MURDER_ON_THE_LINKS,
+    BOOK_AC_THE_MYSTERIOUS_AFFAIR_AT_STYLES,
+    BOOK_CD_THE_HOUND_OF_THE_BASKERVILLES,
+]
+
+
+
+# GENERIC_PATTERNS = {
+#     'detective':    r"detective|investigator|inspector",
+#     'perpetrator':  r"perpetrator|killer|criminal|murderer|culprit|evildoer|offender|villian",
+#     'victim':       r"victim|casualty",
+#     'crime':        r"crime|kill|murder|dead|offense|misdeed",
+#     'suspect':      r"suspect|accused|defendant",
+# }
+
+
+
+# Prompts:
+# 1) When does the investigator (or a pair) occur for the first time?
+# 2) When is the crime first mentioned? - the type of the crime and the details
+# 3) When is the perpetrator first mentioned?
+# 4) What are the three words that occur around the perpetrator on each mention?
+#    (i.e., the three words preceding and the three words following the mention of a perpetrator)
+# 5) When and how the detective/detectives and the perpetrators co-occur?
+# 6) When are other suspects first introduced?
+# Need:
+#   Every Match: {detectives, perpetrators}
+#   First Match: {crime, suspects, victims}
+
+
+def find_first_book_match(content: list[list[str]], pattern: re.Pattern):
+    for idxs, st in iter_book_content(content):
+        if (match := pattern.search(st)) is not None:
+            return (idxs, match)
+    return None
+
+
+def find_all_book_matches(content: list[list[str]], pattern: re.Pattern):
+    results = {}
+    for idxs, st in iter_book_content(content):
+        if (match := pattern.search(st)) is not None:
+            results[idxs] = match
+    return results
+
+
+def process_book(book: dict):
+    results = {}
+
+    for category in ('detectives', 'perpetrators'):
+        matches = results[category] = {}
+        for k, re_k in book[category].items():
+            re_k = re.compile(rf"\b(?:{re_k})\b", re.I | re.X)
+            matches[k] = find_all_book_matches(book['content'], re_k)
+
+    for category in ('suspects', 'victims'):
+        matches = results[category] = {}
+        for k, re_k in book[category].items():
+            re_k = re.compile(rf"\b(?:{re_k})\b", re.I | re.X)
+            matches[k] = find_first_book_match(book['content'], re_k)
+
+    re_crime = re.compile(rf"\b(?:{book['crime']})\b", re.I | re.X)
+    results['crime'] = find_first_book_match(book['content'], re_crime)
+
+    return results
+
+
+
+
+
+
+### Perform book processing at module import
+
+for book in BOOKS:
+    book['matches'] = process_book(book)
--- src/read_books.py
+++ src/read_books.py
@@ -2,26 +2,34 @@
 import re
 
 
+# _re_quotes = re.compile(r"\"|'(?!\S)|(?<!\S)'")
+_re_quotes = re.compile(r"[\"']")
+_re_puncts = re.compile(r'(?<![MDF]r|Ms|St)(?<!Mrs)(?<![A-Z])\.|[,?!:;]')
+_re_sentence_delim = re.compile(r"""
+    (?:(?<=\S[.?!])|(?<=\S[.?!]["']))
+    (?<!Mrs\.)(?<![MDF]r\.|Ms\.|St\.)(?<![A-Z]\.)  \s  (?=[A-Z])
+""", re.VERBOSE)
+
+def split_sentences(pg: str):
+    ss = _re_sentence_delim.split(pg)
+    ss = [_re_quotes.sub('', s) for s in ss]    # remove quotes around text (and after plural possessives)
+    ss = [_re_puncts.sub('', s) for s in ss]    # remove punctuation
+    return ss
+
 
 def read_book(filename: str):
-    """ Read in a book text file and tokenize paragraphs in chapters.
+    """ Read in a book file and tokenize text as chapters of sentences.
 
-    Params:
-        filename: string containing path to book file
-
-    Returns:
-        tuple(header, chapters)
-            header: List of header lines
-            chapters: 2D List of text as [chapter][paragraph]
+    Returns: Tuple[header, chapters]
+        header:   List of header lines
+        chapters: 2D List of [chapters][sentences] (first sentence is chapter header)
     """
     header = []
     chapters = []
 
     with open(filename, 'rt') as fin:
         # read the header
-        for line in fin:
-            if line.isspace():
-                break
+        while not (line := next(fin)).isspace():
             header.append(line)
 
         # split the book into chapters of paragraph lines
@@ -29,13 +37,41 @@ def read_book(filename: str):
             chap = [p for p in re.split(r"(?:\n[^\S\r\n]*){2,3}", c) if len(p) and not p.isspace()]
             chapters.append(chap)
 
-    # combine the paragraph lines
-    re_subsp = re.compile(r'\r?\n|\s+')
+    # Combine the paragraph lines. Then, split into sentences
+    re_garbage = re.compile(r'[\[\]{}]()')
+    re_dashes  = re.compile(r'(?<![^\s-])-|-(?![^\s-])')
+    re_subsp   = re.compile(r'[—_\r\n\s]+')
+    re_dquotes = re.compile(r"[“”]")
+    re_squotes = re.compile("[‘’]")
     for i,c in enumerate(chapters):
         for j,p in enumerate(c):
-            chapters[i][j] = re_subsp.sub(' ', p)
+            p = re_garbage.sub('',  p)
+            p = re_dashes .sub(' ', p)
+            p = re_subsp  .sub(' ', p)
+            p = re_dquotes.sub('"', p)
+            p = re_squotes.sub("'", p)
+            chapters[i][j] = split_sentences(p) if (j > 0) else [p]
+
+    # flatten chapter paragraphs to sentences
+    chapters = [[st for pg in ch for st in pg] for ch in chapters]
 
     return header, chapters
+
+
+def iter_book_content(content: list[list[str]]):
+    """ Return generator for book content 3D list.
+        First sentence is skipped to ignore header.
+
+    Yields: Tuple[index, sencence]
+        index: Tuple[i_chapter, i_sentence]
+        sentence: str
+    """
+    for ch_idx, ch in enumerate(content):
+        st_iter = iter(ch)
+        ch_title = next(st_iter)
+        for st_idx, st in enumerate(st_iter, 1):
+            yield ((ch_idx, st_idx), st)
+
 
 
 def analyze_book(content: list, book_no: int):
@@ -55,7 +91,7 @@ def analyze_book(content: list, book_no: int):
         book_mysterious_victim_pattern = rf'({victim_pattern}|(Paul(\sRenauld)?))'
         book_mysterious_crime_pattern = rf'({crime_pattern}|grave|(golf(\scourse)?)|stabbed)'
         book_mysterious_suspect_pattern = rf'({suspect_pattern}|((Madame\s)?Virginie(\sRenauld)?)|(Jack(\sRenauld)?)|(Marthe(\sDaubreuil)?)|Daubreuil|(Monsieur(\sBex)?)|Bex|((Mademoiselle\s)?Dulcie(\sDuveen)?)|Duveen|Cinderella)'
-    
+
     elif(book_no == 2):
         # Key words for The Mysterious Affair at Styles
         print("----------Analysis for The Mysterious Affair at Styles----------")
@@ -64,7 +100,6 @@ def analyze_book(content: list, book_no: int):
         book_mysterious_victim_pattern = rf'({victim_pattern}|(Emily(\sInglethorp)?)|Inglethorp|widow)'
         book_mysterious_crime_pattern = rf'({crime_pattern}|strychnine|poison)'
         book_mysterious_suspect_pattern = rf'({suspect_pattern}|(John(\sCavendish)?)|Cavendish|(Mary(\sCavendish)?)|(Lawrence(\sCavendish)?)|(Cynthia(\sMurdoch)?)|Murdoch|(Dr\.(\sBauerstein)?)|Bauerstein)'
-        total_chapters -= 1
 
     elif(book_no == 3):
         # Key words for The Hound of the Baskervilles
